ca80faca08e92ecf122c4a9917e528dabec66b3a
Hodzinets <godzinets35@gmail.com>
2021-12-01 18:34:34 +0100

    Added eureka configuration

--- aggregator/src/main/java/com/pocoda/aggregator/AggregatorApplication.java
+++ aggregator/src/main/java/com/pocoda/aggregator/AggregatorApplication.java
@@ -2,8 +2,10 @@ package com.pocoda.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AggregatorApplication {
 
 	public static void main(String[] args) {
